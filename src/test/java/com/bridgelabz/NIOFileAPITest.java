a9160234c8d1e724634a3ef6508f4abec2444365
dhirajp504@gmail.com <dhirajp504@gmail.com>
2021-08-03 17:56:28 +0530

    Added: Functionality to Read and Write data to Console

---
+++ src/test/java/com/bridgelabz/NIOFileAPITest.java
@@ -0,0 +1,78 @@
+package com.bridgelabz;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class NIOFileAPITest {
+	public static final String HOME = System.getProperty("user.home");
+	public static final String PLAY_WITH_NIO = "TempPlayground";
+
+	@Test
+	public void givenPath_whenChecked_thenConfirm() throws IOException {
+		// check file Exists
+		Path homePath = Paths.get(HOME);
+		Assert.assertTrue(Files.exists(homePath));
+
+		// Delete file and check file not exist
+		Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		if (Files.exists(playPath))
+			FileUtils.deleteFiles(playPath.toFile());
+		Assert.assertTrue(Files.notExists(playPath));
+
+		// Create Directory
+		Files.createDirectory(playPath);
+		Assert.assertTrue(Files.exists(playPath));
+
+		// create file
+		IntStream.range(1, 10).forEach(counter -> {
+			Path tempFile = Paths.get(playPath + "/temp" + counter);
+			Assert.assertTrue(Files.notExists(tempFile));
+
+			File myFile = new File(tempFile.toString());
+			try {
+				Files.createFile(tempFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			Assert.assertTrue(Files.exists(tempFile));
+		});
+
+		Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+		Files.newDirectoryStream(playPath).forEach(System.out::println);
+		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
+				.forEach(System.out::println);
+
+	}
+
+	@Test
+	void givenDirectory_WhenWatched_listsAllTheActivites() throws IOException {
+		Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new FileWatchService(dir).processEvents();
+	}
+
+	@Test
+	public void numberOfEmployeeEntryTest() {
+		EmployeePayrollData[] empArray = { new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+				new EmployeePayrollData(2, "Bill Gates", 200000.0),
+				new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0) };
+		EmployeePayrollServices employeePayrollService = new EmployeePayrollServices(Arrays.asList(empArray));
+		employeePayrollService.writeEmployeeData(EmployeePayrollServices.IOService.FILE_IO);
+		employeePayrollService.printData(EmployeePayrollServices.IOService.FILE_IO);
+
+		long entries = employeePayrollService.countEntries(EmployeePayrollServices.IOService.FILE_IO);
+		System.out.println("No.of entries into file are: " + entries);
+
+		Assert.assertEquals(3, entries);
+	}
+}
